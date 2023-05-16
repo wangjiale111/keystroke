31830a232e432ae0d250463322f4fb51cb4390e3
王佳乐 <747934484@qq.com>
2023-05-16 17:29:41 +0800

    update: 折线图

--- src/record/ViewModelPlayBack.ts
+++ src/record/ViewModelPlayBack.ts
@@ -1,10 +1,9 @@
-import { UserViewModel } from './DomEventRecord';
-import { CurriculumViewModelClasskey } from '../components/WritingRecordViewModel';
-
+import {UserViewModel} from './DomEventRecord';
+import {CurriculumViewModelClasskey} from '../components/WritingRecordViewModel';
 
 
 export class ViewModelPlayBack {
- 
+
     userViewModelLog = null;
     runSimulator = -1;
 
@@ -13,73 +12,84 @@ export class ViewModelPlayBack {
     }
 
 
-    start = ( finish?:any) => {
-            if ((window as any).playbackInProgress !== false) {
-                throw new Error('无法启动回放--另一个回放◀已在进行中。');
-                return;
-            }
+    start = (finish?: any) => {
+        if ((window as any).playbackInProgress !== false) {
+            throw new Error('无法启动回放--另一个回放◀已在进行中。');
+            return;
+        }
 
-            if ((window as any).recordInProgress !== false) {
-                throw new Error('无法启动回放--另一个录制正在进行中。');
-                return;
-            }
+        if ((window as any).recordInProgress !== false) {
+            throw new Error('无法启动回放--另一个录制正在进行中。');
+            return;
+        }
 
-           /* if (window.innerHeight !== (window as any).height || window.innerWidth !== (window as any).width) {
-                throw new Error('无法启动回放--浏览器窗口必须与录制播放脚本的宽高匹配 (' + (window as any).width + 'px by ' + (window as any).height + 'px). Window is currently ' + window.innerWidth + 'px by ' + window.innerHeight + 'px.');
-                return;
-            }*/
+        /* if (window.innerHeight !== (window as any).height || window.innerWidth !== (window as any).width) {
+             throw new Error('无法启动回放--浏览器窗口必须与录制播放脚本的宽高匹配 (' + (window as any).width + 'px by ' + (window as any).height + 'px). Window is currently ' + window.innerWidth + 'px by ' + window.innerHeight + 'px.');
+             return;
+         }*/
 
 
-            console.log('Starting test script playback.');
+        console.log('Starting test script playback.');
 
-            (window as any).playbackInProgress = true;
+        (window as any).playbackInProgress = true;
 
-            const timeStartedPlayback = new Date().getTime();
+        const timeStartedPlayback = new Date().getTime();
 
         this.runSimulator = setInterval(() => {
 
-                const currentTime = new Date().getTime();
-                let userEventLength = (this.userViewModelLog as any).length;
-            if (currentTime > ((this.userViewModelLog as any)[0].timeStamp + timeStartedPlayback)) {
-                    do {
-                        // 将timetemp为0的处理完后，回调取消loading，开始正常回放
-                        if((this.userViewModelLog as any)[0].timeStamp >= 1000 && finish && typeof(finish) == 'function'){
-                            finish();
-                            finish = null;
-                        }
-                        const userEvent = (this.userViewModelLog as any).splice(0, 1)[0] as UserViewModel;
-                        userEventLength--;
-                        /**
-                         * 接收类，定义唯一classKey
-                         * emitter.on('foo', e => console.log('foo', e) )
-                         */
+            const currentTime = new Date().getTime();
+            let userEventLength = (this.userViewModelLog as any).length;
+            // q: 为什么报错：k.ts:43  Uncaught TypeError: Cannot read properties of undefined (reading 'timeStamp') at eval (ViewModelPlayBack.
+            // a: 因为userEventLength为0，所以(this.userViewModelLog as any)[0]为undefined
+            // q: 如何解决？
+            // a: 在userEventLength为0时，不执行下面的代码
 
-                        // 发送消息给对应的vue class
-                        (window as any).emitter.emit('Writing', { key: userEvent.modelKey, classKey: userEvent.classKey, value: userEvent.modelValue, timeStamp : userEvent.timeStamp });
-                    } while (userEventLength > 0 && ((currentTime + 50) > ((this.userViewModelLog as any)[0].timeStamp + timeStartedPlayback)));
-                }
             // 无回放数据的时候取消loading
-                if (userEventLength == 0) {
-                    if( finish && typeof(finish) == 'function'){
-                        finish();
-                        finish = null;
-                    }
-                    clearInterval(this.runSimulator);
-                    console.debug('Test script playback finished.');
-                    (window as any).playbackInProgress = false;
+            if (userEventLength == 0) {
+                if (finish && typeof (finish) == 'function') {
+                    finish();
+                    finish = null;
                 }
-               /* // 当前无回放回放数据取消loading
-                if(userEventLength > 0 && ((currentTime + 50) <= ((this.userViewModelLog as any)[0].timeStamp + timeStartedPlayback))){
-                    if( finish && typeof(finish) == 'function'){
-                        console.debug('888888888888888888888888888' );
+                clearInterval(this.runSimulator);
+                console.debug('Test script playback finished.');
+                (window as any).playbackInProgress = false;
+            }
+
+            if (currentTime > ((this.userViewModelLog as any)[0].timeStamp + timeStartedPlayback)) {
+                do {
+                    // 将timetemp为0的处理完后，回调取消loading，开始正常回放
+                    if ((this.userViewModelLog as any)[0].timeStamp >= 1000 && finish && typeof (finish) == 'function') {
                         finish();
                         finish = null;
                     }
-                }*/
-            }, 10);
+                    const userEvent = (this.userViewModelLog as any).splice(0, 1)[0] as UserViewModel;
+                    userEventLength--;
+                    /**
+                     * 接收类，定义唯一classKey
+                     * emitter.on('foo', e => console.log('foo', e) )
+                     */
+
+                    // 发送消息给对应的vue class
+                    (window as any).emitter.emit('Writing', {
+                        key: userEvent.modelKey,
+                        classKey: userEvent.classKey,
+                        value: userEvent.modelValue,
+                        timeStamp: userEvent.timeStamp
+                    });
+                } while (userEventLength > 0 && ((currentTime + 50) > ((this.userViewModelLog as any)[0].timeStamp + timeStartedPlayback)));
+            }
+            /* // 当前无回放回放数据取消loading
+             if(userEventLength > 0 && ((currentTime + 50) <= ((this.userViewModelLog as any)[0].timeStamp + timeStartedPlayback))){
+                 if( finish && typeof(finish) == 'function'){
+                     console.debug('888888888888888888888888888' );
+                     finish();
+                     finish = null;
+                 }
+             }*/
+        }, 10);
 
     }
-    stop = () =>{
+    stop = () => {
         clearInterval(this.runSimulator);
         this.userViewModelLog = null;
         console.debug('Test script playback stop.');
--- src/views/ReplayView.vue
+++ src/views/ReplayView.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="record">
-    <div class="writing">
-      <textarea id="text" cols="100" rows="10" v-model="value" ></textarea>
-      <br>
+    <div class="record">
+        <div class="writing">
+            <textarea id="text" cols="100" rows="10" v-model="value"></textarea>
+            <br>
+        </div>
     </div>
-  </div>
-  <div class="play">
-    <div>
-      <button @click="Replay">开始回放</button>
-      <button @click="exitReplay">退出回放</button>
+    <div class="play">
+        <div>
+            <button @click="Replay">开始回放</button>
+            <button @click="exitReplay">退出回放</button>
+        </div>
+        <div class="index">写作速度:{{ typeSpeed }}字/秒</div>
+        <div class="index">写作总时间:{{ time }}</div>
+        <div class="index">写作总字数:{{ writingLength }}</div>
     </div>
-    <div class="index">写作速度:{{typeSpeed}}字/秒</div>
-    <div class="index">写作总时间:{{time}}</div>
-    <div class="index">写作总字数:{{writingLength}}</div>
-  </div>
-  <div id="chart" style="width: 100%;height: 300px;"></div>
+    <div id="chart" style="width: 100%;height: 300px;"></div>
 </template>
 
 
@@ -25,193 +25,206 @@ import {DomEventRecord} from "@/record/DomEventRecord";
 import index from "@/store"
 import * as echarts from 'echarts';
 
-@Options({
-
-})
+@Options({})
 
 export default class ReplayView extends mixins(ReplayViewModel) {
 
-  viewModelPlayback: any;
-  domRecord = new DomEventRecord();
-  replayData:any;
-  value = '';
-  typeNum = 0;
-  typeLength = 0;
-  startTime = 0;
-  typeTime = 0;
-  typeSpeed = 0;
-  allTime = 0;
-  time='00分00秒';
-  writingLength=0;
-  flag = 1;
-  timing : any;
-  chart: any;
+    viewModelPlayback: any;
+    domRecord = new DomEventRecord();
+    replayData: any;
+    value = '';
+    typeNum = 0;
+    typeLength = 0;
+    startTime = 0;
+    typeTime = 0;
+    typeSpeed = 0;
+    allTime = 0;
+    time = '00分00秒';
+    writingLength = 0;
+    flag = 1;
+    timing: any;
+    chart: any;
+    timeArray: any[] = [];
+    speedArray: any[] = [];
 
-  async created() {
-    (window as any).playbackInProgress = false;
-    // await this.getReplayData(this.time);
-  }
-
-  Replay(){
-    this.replayData = index.state.data
-    this.startTime = 0
-    // 计时器
-    clearInterval(this.timing);
-    this.timing = setInterval(() => {
-      if (this.flag == 1) {
-        this.allTime ++
-      }
-      this.time = this.formateSeconds(this.allTime)
-    }, 1000);
-    // 每隔三秒算一次速度
-    // this.typeNum = this.value.length
-    // setInterval(() => {
-    //     this.typeNum = this.value.length - this.typeNum
-    //     this.typeSpeed =  Math.floor((this.typeNum / 3) * 100) / 100;
-    // }, 3000)
-    if ((window as any).emitter) {
-      (window as any).emitter.on('Writing', (data: any) => {
-        this.viewModelPlaBackHander(data);
-      });
-    }
-    if (this.replayData.length) {
-      this.viewModelPlayback = this.domRecord.startViewModelPlayback(this.replayData);
-    }
-    this.flag = 1;
-  }
-
-  /**
-   * 退出回放
-   */
-  exitReplay() {
-    this.flag = 0;
-    try {
-      this.domRecord.stopViewModelPlayback(this.viewModelPlayback).then();
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  //将秒转化为时分秒
-  formateSeconds(endTime:any) {
-    let secondTime = parseInt(endTime); //将传入的秒的值转化为Number
-    let min = 0; // 初始化分
-    let h = 0; // 初始化小时
-    let result = "";
-    if (secondTime > 60) {
-      //如果秒数大于60，将秒数转换成整数
-      min = parseInt(String(secondTime / 60)); //获取分钟，除以60取整数，得到整数分钟
-      secondTime = parseInt(String(secondTime % 60)); //获取秒数，秒数取佘，得到整数秒数
-      if (min > 60) {
-        //如果分钟大于60，将分钟转换成小时
-        h = parseInt(String(min / 60)); //获取小时，获取分钟除以60，得到整数小时
-        min = parseInt(String(min % 60)); //获取小时后取佘的分，获取分钟除以60取佘的分
-      }
-    }
-    if (h.toString().padStart(2, "0") == "00") {
-      result = `${min.toString().padStart(2, "0")}分${secondTime
-          .toString()
-          .padStart(2, "0")}秒`;
-    } else {
-      result = `${h.toString().padStart(2, "0")}时${min
-          .toString()
-          .padStart(2, "0")}分${secondTime.toString().padStart(2, "0")}秒`;
+    async created() {
+        (window as any).playbackInProgress = false;
+        // await this.getReplayData(this.time);
     }
 
-    return result;
-  }
-
-  viewModelPlaBackHander(data:any){
-    // 把值赋给value双向绑定
-    this.value = data.value;
-    this.writingLength = this.value.length
-    console.log(data);
-    // 计算当前时间戳内打字数目
-    if (data.value.length > this.typeLength) {
-      this.typeNum = data.value.length - this.typeLength
-      // console.log('打字数：' + this.typeNum)
-    } else {
-      this.typeNum = 0
+    Replay() {
+        this.replayData = index.state.data
+        this.startTime = 0
+        // 计时器
+        clearInterval(this.timing);
+        this.timing = setInterval(() => {
+            if (this.flag == 1) {
+                this.allTime++
+            }
+            this.time = this.formateSeconds(this.allTime)
+        }, 1000);
+        // 每隔三秒算一次速度
+        // this.typeNum = this.value.length
+        // setInterval(() => {
+        //     this.typeNum = this.value.length - this.typeNum
+        //     this.typeSpeed =  Math.floor((this.typeNum / 3) * 100) / 100;
+        // }, 3000)
+        if ((window as any).emitter) {
+            (window as any).emitter.on('Writing', (data: any) => {
+                this.viewModelPlaBackHander(data);
+            });
+        }
+        if (this.replayData.length) {
+            this.viewModelPlayback = this.domRecord.startViewModelPlayback(this.replayData);
+        }
+        this.flag = 1;
     }
-    // 时间戳的差作为时间
-    this.typeTime = (data.timeStamp - this.startTime) / 1000
-    // console.log(this.typeTime)
-    // 计算速度
-    if (this.typeNum === 0) {
-      this.typeSpeed = 0
-    } else {
-      this.typeSpeed =  Math.floor((this.typeNum / this.typeTime) * 100) / 100;
-    }
-    // console.log('打字速度：' +this.typeSpeed)
-    // 记录一下当前的时间戳和字符长度，以便下一次使用
-    this.startTime = data.timeStamp
-    this.typeLength = data.value.length
 
-    // echarts
-    if (!this.chart) {
-      this.chart = echarts.init(document.getElementById('chart'));
-      this.chart.setOption({
-        title: {
-          text: '打字速度'
-        },
-        tooltip: {
-          trigger: 'axis'
-        },
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: [this.allTime]
-        },
-        yAxis: {
-          type: 'value',
-          boundaryGap: [0, '100%']
-        },
-        series: [{
-          name: '速度',
-          type: 'line',
-          data: [this.typeSpeed]
-        }]
-      });
-    } else {
-      this.chart.setOption({
-        xAxis: {
-          data: [this.allTime]
-        },
-        series: [{
-          name: '速度',
-          data: [this.typeSpeed]
-        }]
-      });
+    /**
+     * 退出回放
+     */
+    exitReplay() {
+        this.flag = 0;
+        try {
+            this.domRecord.stopViewModelPlayback(this.viewModelPlayback).then();
+        } catch (e) {
+            console.error(e);
+        }
     }
-  }
 
-  // getReplayData(time: number) {
-  //     this.replayData = index.state.data
-  //     this.replayVMData = [];
-  //     for (let i = 0; i < this.replayData.length; i++){
-  //         const temp = {
-  //             classKey: this.replayData[i].classKey,
-  //             timeStamp: this.replayData[i].timeStamp,
-  //             modelKey: this.replayData[i].modelKey,
-  //             modelValue: this.replayData[i].value
-  //         };
-  //         this.replayVMData.push(temp);
-  //         i += 1;
-  //     }
-  // }
+    //将秒转化为时分秒
+    formateSeconds(endTime: any) {
+        let secondTime = parseInt(endTime); //将传入的秒的值转化为Number
+        let min = 0; // 初始化分
+        let h = 0; // 初始化小时
+        let result = "";
+        if (secondTime > 60) {
+            //如果秒数大于60，将秒数转换成整数
+            min = parseInt(String(secondTime / 60)); //获取分钟，除以60取整数，得到整数分钟
+            secondTime = parseInt(String(secondTime % 60)); //获取秒数，秒数取佘，得到整数秒数
+            if (min > 60) {
+                //如果分钟大于60，将分钟转换成小时
+                h = parseInt(String(min / 60)); //获取小时，获取分钟除以60，得到整数小时
+                min = parseInt(String(min % 60)); //获取小时后取佘的分，获取分钟除以60取佘的分
+            }
+        }
+        if (h.toString().padStart(2, "0") == "00") {
+            result = `${min.toString().padStart(2, "0")}分${secondTime
+                .toString()
+                .padStart(2, "0")}秒`;
+        } else {
+            result = `${h.toString().padStart(2, "0")}时${min
+                .toString()
+                .padStart(2, "0")}分${secondTime.toString().padStart(2, "0")}秒`;
+        }
+
+        return result;
+    }
+
+    viewModelPlaBackHander(data: any) {
+        // 把值赋给value双向绑定
+        this.value = data.value;
+        this.writingLength = this.value.length
+        console.log(data);
+        // 计算当前时间戳内打字数目
+        if (data.value.length > this.typeLength) {
+            this.typeNum = data.value.length - this.typeLength
+            // console.log('打字数：' + this.typeNum)
+        } else {
+            this.typeNum = 0
+        }
+        // 时间戳的差作为时间
+        this.typeTime = (data.timeStamp - this.startTime) / 1000
+        // console.log(this.typeTime)
+        // 计算速度
+        if (this.typeNum === 0) {
+            this.typeSpeed = 0
+        } else {
+            this.typeSpeed = Math.floor((this.typeNum / this.typeTime) * 100) / 100;
+        }
+        this.speedArray.push(this.typeSpeed);
+        this.timeArray.push(this.allTime);
+        // console.log('打字速度：' +this.typeSpeed)
+        // 记录一下当前的时间戳和字符长度，以便下一次使用
+        this.startTime = data.timeStamp
+        this.typeLength = data.value.length
+
+        if (!this.chart) {
+            this.chart = echarts.init(document.getElementById('chart'));
+            this.chart.setOption(
+                {
+                    title: {
+                        text: '打字速度'
+                    },
+                    tooltip: {
+                        trigger: 'axis'
+                    },
+                    xAxis: {
+                        type: 'category',
+                        boundaryGap: false,
+                        data: this.timeArray,
+                        axisLabel: {
+                            formatter: '{value} 秒'
+                        }
+                    },
+                    yAxis: {
+                        type: 'value',
+                        axisLabel: {
+                            formatter: '{value} 字/秒'
+                        }
+                    },
+                    series: [
+                        {
+                            name: '速度',
+                            type: 'line',
+                            data: this.speedArray
+                        }
+                    ]
+                }
+            );
+
+
+        } else {
+            this.chart.setOption({
+                xAxis: {
+                    data: this.timeArray
+                },
+                series: [{
+                    name: '速度',
+                    data: this.speedArray
+                }]
+            });
+        }
+    }
+
+    // getReplayData(time: number) {
+    //     this.replayData = index.state.data
+    //     this.replayVMData = [];
+    //     for (let i = 0; i < this.replayData.length; i++){
+    //         const temp = {
+    //             classKey: this.replayData[i].classKey,
+    //             timeStamp: this.replayData[i].timeStamp,
+    //             modelKey: this.replayData[i].modelKey,
+    //             modelValue: this.replayData[i].value
+    //         };
+    //         this.replayVMData.push(temp);
+    //         i += 1;
+    //     }
+    // }
 }
 </script>
 
 <style scoped>
-.play{
-  height: 300px;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  justify-content: center;
+.play {
+    height: 300px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: center;
 }
-.index{
-  margin-left: 60px;
+
+.index {
+    margin-left: 60px;
 }
 </style>
 
